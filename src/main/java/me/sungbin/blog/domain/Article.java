d6dd9766ef5fca243805687bd1db95c5f92ddb1b
code-tree <dltjdqls1200@naver.com>
2023-06-04 13:10:15 +0900

    test: 게시글 저장 테스트 작성

--- src/main/java/me/sungbin/blog/domain/Article.java
+++ src/main/java/me/sungbin/blog/domain/Article.java
@@ -1,10 +1,6 @@
 package me.sungbin.blog.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- src/test/java/me/sungbin/blog/controller/BlogRestControllerTest.java
+++ src/test/java/me/sungbin/blog/controller/BlogRestControllerTest.java
@@ -1,14 +1,22 @@
 package me.sungbin.blog.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.sungbin.blog.controller.dto.AddArticleRequest;
+import me.sungbin.blog.domain.Article;
 import me.sungbin.blog.repository.BlogRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,10 +29,28 @@ class BlogRestControllerTest {
     protected ObjectMapper objectMapper;
 
     @Autowired
-    private WebApplicationContext context;
-
-    @Autowired
     BlogRepository blogRepository;
 
+    @DisplayName("게시글 작성에 성공한다.")
+    @Test
+    public void addArticle() throws Exception {
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+        final AddArticleRequest articleRequest = new AddArticleRequest(title, content);
 
+        final String requestBody = objectMapper.writeValueAsString(articleRequest);
+
+        var result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        result.andExpect(status().isCreated());
+
+        final List<Article> articles = blogRepository.findAll();
+
+        assertThat(articles.size()).isEqualTo(1);
+        assertThat(articles.get(0).getTitle()).isEqualTo(title);
+        assertThat(articles.get(0).getContent()).isEqualTo(content);
+    }
 }
